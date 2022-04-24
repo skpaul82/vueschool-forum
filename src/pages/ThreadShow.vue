d2b9c958a4e10dd79c59bc5076e9c5f0fb1ac56d
skpaul82 <skpaul82@gmail.com>
2022-04-25 03:21:15 +0600

    Add new post form abd update dataScouce.

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,6 +3,24 @@
     <h1>{{ thread.title }}</h1>
 
     <PostList :posts="threadPosts"/>
+
+    <div class="">
+      <form @submit.prevent="addPost">
+        <div class="form-group row">
+          <label for="newPost" class="col-sm-1-12 col-form-label"></label>
+          <div class="col-sm-1-12">
+            <textarea v-model="newPost" class="form-control" name="newPost" id="newPost" placeholder="Write here.."></textarea>
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="offset-sm-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -23,7 +41,8 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts
+      posts: sourceData.posts,
+      newPost: null
     }
   },
   computed: {
@@ -32,6 +51,21 @@ export default {
     },
     threadPosts () {
       return this.posts.filter(post => post.threadId === this.id)
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'aaa' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPost,
+        publishdAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2'
+      }
+      this.posts.push(post)
+      this.thread.posts.push(postId)
+      this.newPost = null
     }
   }
 }
