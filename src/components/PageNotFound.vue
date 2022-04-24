7a87fd38f8a6088bfdfafb065c7d38a1db1b02dd
skpaul82 <skpaul82@gmail.com>
2022-04-24 23:42:22 +0600

    - Create 404 NotFound Component, page and route - Add router link to thread

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="">Not Found</h1>
+        <router-link :to="{name: 'Home'}">Read some cool theads</router-link>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
